8d1b1aecb3b95db8181d099ed0de82bd788c9dfe
Lukhausen <mail@lukhausen.de>
2025-04-14 02:59:34 +0200

    h

--- experimental/marc/TinyZero/scripts/train_tiny_zero_7b_medium_optimized.sh
+++ experimental/marc/TinyZero/scripts/train_tiny_zero_7b_medium_optimized.sh
@@ -5,6 +5,7 @@ export ROLLOUT_TP_SIZE=1
 export EXPERIMENT_NAME=arc_agi_two-qwen2.5-7B
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export WANDB_API_KEY="724a473f54d00ed3c8fab36dc7abf32c23523360"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 
 
@@ -20,12 +21,12 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.grad_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
@@ -35,6 +36,9 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.ref.log_prob_micro_batch_size=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    critic.model.fsdp_config.param_offload=True \
+    critic.model.fsdp_config.grad_offload=True \
+    critic.model.fsdp_config.optimizer_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['wandb'] \
